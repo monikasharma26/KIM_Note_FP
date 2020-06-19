66d205d181bb07f86202f70fa055017b9a5c7cc9
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-06-19 18:25:11 -0400

    [KRITIMA] Variables added in note class

---
+++ KIM_Note_FP/Model/Note.swift
@@ -0,0 +1,21 @@
+//
+//  Note.swift
+//  KIM_Note_FP
+//
+//  Created by Kritima Kukreja on 2020-06-19.
+//  Copyright © 2020 S@i. All rights reserved.
+
+import UIKit
+
+class Note: NSObject, NSCoding {
+    
+    // MARK: Variables
+
+    var title: String?
+    var content: UITextView?
+    var uuid: String
+    var editDate: NSDate?
+    
+    // MARK: Constructors
+    
+   
