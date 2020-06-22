77688e683239d6fcf643e7e887837a224ab31ee6
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-06-22 12:36:02 -0400

    Note View Controller added

---
+++ KIM_Note_FP/FolderTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FolderTableViewController.swift
+//  KIM_Note_FP
+//
+//  Created by Kritima Kukreja on 2020-06-22.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+
+class FolderTableViewController: UITableViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+}
+}
---
+++ KIM_Note_FP/NoteViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NoteTableViewCell.swift
+//  KIM_Note_FP
+//
+//  Created by Kritima Kukreja on 2020-06-19.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+
+class NoteViewController: UIViewController {
+
+    var selectedNote: Note? {
+        didSet {
+            
+        }
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
