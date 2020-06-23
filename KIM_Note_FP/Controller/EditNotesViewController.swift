91c5be7d1404d44690c80d84a0228934f204c012
Roopvirk6 <virkikroop13@gmail.com>
2020-06-23 10:00:54 -0400

    [ikroop] image button added

--- KIM_Note_FP/Controller/EditNotesViewController.swift
+++ KIM_Note_FP/Controller/EditNotesViewController.swift
@@ -9,14 +9,51 @@
 import UIKit
 
 class EditNotesViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var selectImage: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    
+    
+    @IBAction func btnImage(_ sender: UIButton)
+    {
+        
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = true
+        
+        let alertController = UIAlertController(title: "Add an Image", message: "Choose From", preferredStyle: .actionSheet)
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (action) in
+            pickerController.sourceType = .camera
+            self.present(pickerController, animated: true, completion: nil)
+            }
+        
+        let photosLibraryAction = UIAlertAction(title: "Photos Library", style: .default) { (action) in
+            pickerController.sourceType = .photoLibrary
+            self.present(pickerController, animated: true, completion: nil)
+            
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alertController.addAction(cameraAction)
+        alertController.addAction(photosLibraryAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
