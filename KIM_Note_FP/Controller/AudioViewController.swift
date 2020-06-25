b3e56b28e90eaaad8186fdc5844944098b1394f8
Philip <jhasondeleon@gmail.com>
2020-06-25 00:43:58 -0400

    [Philip] audio view controller added

---
+++ KIM_Note_FP/Controller/AudioViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AudioViewController.swift
+//  KIM_Note_FP
+//
+//  Created by user175490 on 6/25/20.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+
+class AudioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
