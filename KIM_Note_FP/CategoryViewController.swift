6c73b0e4e2a00d98fc8331716d1cadf7a2ad84c6
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-06-25 14:11:23 -0400

    [KRITIMA] Core Data initialization added in category view controller

---
+++ KIM_Note_FP/CategoryViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryViewController.swift
+//  KIM_Note_FP
+//
+//  Created by Kritima Kukreja on 2020-06-25.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var objects = [Any]()
+    var managedContext: NSManagedObjectContext!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // check if all category is present if not add it
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard(_:)))
+//        self.view.addGestureRecognizer(tapGesture)
+//
+         // Core data initialization
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            // create alert
+            let alert = UIAlertController(
+                title: "Could note get app delegate",
+                message: "Could note get app delegate, unexpected error occurred. Try again later.",
+                preferredStyle: .alert)
+            // add OK action
+            alert.addAction(UIAlertAction(title: "OK",
+                                          style: .default))
+            // show alert
+            self.present(alert, animated: true)
+            return
+        }
+        managedContext = appDelegate.persistentContainer.viewContext
+         
