7dc3331b4eddafca176a7018a227120b000092a1
monikasharma26 <monikasharma26081991@gmail.com>
2020-06-27 05:06:04 -0400

    [Monika] Location Updated

--- KIM_Note_FP/Controllers/NoteLocationVC.swift
+++ KIM_Note_FP/Controllers/NoteLocationVC.swift
@@ -7,24 +7,42 @@
 //
 
 import UIKit
-
-class NoteLocationVC: UIViewController {
-
+import  CoreLocation
+import CoreData
+import MapKit
+class NoteLocationVC: UIViewController,CLLocationManagerDelegate, MKMapViewDelegate {
+    var lat:Double?
+    var long:Double?
+    
+    var locationmanager = CLLocationManager()
+    
+    let regionRadius: CLLocationDistance = 1000
+    
+    @IBOutlet var myMapView: MKMapView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        locationmanager.delegate = self
+        locationmanager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationmanager.requestWhenInUseAuthorization()
+        locationmanager.startUpdatingLocation()
+        
+        myMapView.showsUserLocation = true
+     
+        let noteLocation = CLLocation(latitude: lat!, longitude: long!)
+          super.viewDidLoad(); self.navigationController!.setNavigationBarHidden(false, animated: true)
+        self.title = "Note Location"
+        
+        let coordinateRegion = MKCoordinateRegion(center: noteLocation.coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        self.myMapView.setRegion(coordinateRegion, animated: true)
+    
+        let myAnnotation: MKPointAnnotation = MKPointAnnotation()
+        myAnnotation.coordinate = CLLocationCoordinate2DMake(noteLocation.coordinate.latitude, noteLocation.coordinate.longitude);
+        myAnnotation.title = "Note Source"
+        myAnnotation.subtitle = "\(String(describing: lat)),\(String(describing: long))"
+        self.myMapView.addAnnotation(myAnnotation)
+     
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
