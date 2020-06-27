9c29a7407b579692be7346bf105eaba848fad253
monikasharma26 <monikasharma26081991@gmail.com>
2020-06-27 05:04:39 -0400

    [monika] Code added

--- KIM_Note_FP/Model/Note.swift
+++ KIM_Note_FP/Model/Note.swift
@@ -1,56 +1,10 @@
 //
-//  Note.swift
+//  Notes.swift
 //  KIM_Note_FP
 //
-//  Created by Kritima Kukreja on 2020-06-19.
+//  Created by S@i on 2020-06-26.
 //  Copyright © 2020 S@i. All rights reserved.
+//
 
-import UIKit
-
-class Note: NSObject, NSCoding {
-    
-    // MARK: Variables
-
-    var title: String?
-    var content: UITextView?
-    var uuid: String
-    var editDate: NSDate?
-    
-    // MARK: Constructors
-    
-    init(title: String, content: String) {
-           self.title = title
-           self.content = UITextView()
-           self.content?.text = content
-           self.uuid = NSUUID().uuidString
-           self.editDate = NSDate()
-       }
-       
-       // MARK: Data Management Protocols
-           
-           func encodeWithCoder(coder: NSCoder) {
-            coder.encode(content, forKey: "Content")
-            coder.encode(title, forKey: "Title")
-            coder.encode(uuid, forKey: "UUID")
-            coder.encode(editDate, forKey: "EditDate")
-           }
-           
-           required init?(coder: NSCoder) {
-            content = coder.decodeObject(forKey: "Content") as? UITextView
-            title = coder.decodeObject(forKey: "Title") as? String
-            uuid = coder.decodeObject(forKey: "UUID") as! String
-            editDate = coder.decodeObject(forKey: "EditDate") as? NSDate
-               }
-               
-              // MARK: Accessor Methods
-                            
-                            func lastEditDate() -> String {
-                                // TODO: Add Real Code
-                                let date = "Last Edited: " + editDate!.weekdayName + " at " + editDate!.toShortTimeString()
-                                return date
-                            }
-
-           }
-
-
+import Foundation
 
--- KIM_Note_FP/Model/Notes.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Notes.swift
-//  KIM_Note_FP
-//
-//  Created by S@i on 2020-06-26.
-//  Copyright © 2020 S@i. All rights reserved.
-//
-
-import Foundation
